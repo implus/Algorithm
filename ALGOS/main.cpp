54b2e084ebc82ad2934a66687133ee524d9546bc
Li Xiang <674592809@qq.com>
2013-07-03 14:44:57 +0800

    try

---
+++ ALGOS/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: admin
+ *
+ * Created on 2013年7月3日, 下午2:22
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
